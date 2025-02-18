52f2158bf88ee1a0ff313f0ef8bb105c6f3b7e0f
experimentosdev2025 <experimentosdev2023@gmail.com>
2025-02-18 16:35:18 -0300

    arquivos base

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AuthProvider, useAuth } from './src/context/AuthContext';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/Home';
+import RegisterScreen from './src/screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Stack.Navigator>
+      {isAuthenticated ? (
+        <Stack.Screen name="Home" component={HomeScreen} />
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+};
+
+export default App;
